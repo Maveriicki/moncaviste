0f203a7493e7249fe82173411b87531831265f9e
Mavericki <mavericki@Mac.lan>
2026-05-16 23:42:29 +0200

    Add onboarding page

---
+++ app/onboarding/page.tsx
@@ -0,0 +1,8 @@
+export default function OnboardingPage() {
+  return (
+    <main className="p-10">
+      <h1>Onboarding Caviste</h1>
+      <p>Création de votre cave en cours.</p>
+    </main>
+  )
+}
